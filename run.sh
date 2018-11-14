22fed947fd7f55d19fe5235571f2a02d6970453e
nicolas kowenski <zakkg3@gmail.com>
2018-11-14 00:26:50 +0000

    update script

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+printf "verifing Requirements."
+if ! [ `sudo systemctl is-active docker` = "active" ];
+ then
+  echo "We need docker running"
+  exit 1;
+fi
+printf ". Docker OK ."
+command -v docker-compose --version >/dev/null 2>&1 || { echo >&2 "docker-compose not found.  Aborting."; exit 1; }
+printf ". Docker-compose OK .\n"
 echo "Creating secret"
 sed -i "s/{{secret}}/$(docker run --rm sentry config generate-secret-key)/" docker-compose.yml
 echo "Running stack..."
