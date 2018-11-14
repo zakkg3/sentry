dd3413c39e812e54c4b97fe5d95e3a1fc0f7d99e
nicolas kowenski <zakkg3@gmail.com>
2018-11-14 00:33:56 +0000

    fix typo

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ echo "Creating secret"
 sed -i "s/{{secret}}/$(docker run --rm sentry config generate-secret-key)/" docker-compose.yml
 echo "Running stack..."
 docker-compose up -d
-echo "creating Db and User"
+echo "Creating Db and User"
 docker-compose exec sentry sentry upgrade
 echo "Restarting server"
 docker-compose restart sentry
