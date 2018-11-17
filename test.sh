d4112e67798e3a7d548ad5fea1acc8d61edd2c05
nicolas kowenski <zakkg3@gmail.com>
2018-11-17 20:20:42 +0000

    update test

--- test.sh
+++ test.sh
@@ -3,13 +3,15 @@ if [ -z "$1" ]
   then
     echo "No argument supplied, please add the AUTH Token."
     echo "using example, test token."
-    export SENTRY_DSN=http://73f0839c97034a83be55ca6912659767@localhost:9000/2
+    export SENTRY_DSN=http://73f0839c97034a83be55ca6912659767@localhost:8000/1
 else
-  export SENTRY_DSN=$1
+  export SENTRY_DSN=http://$1@localhost:8000/4
 fi
+command -v sentry-cli --version >/dev/null 2>&1 || { echo >&2 "sentry-cli not found.  Aborting."; exit 1; }
 
+echo "Sending a test error..."
 sentry-cli send-event -m "This is a test" -m "Another thing here"
-
+echo "Catching Bash error..."
 eval "$(sentry-cli bash-hook)"
 if [ false ]; then
   this-is-a-bash-error
